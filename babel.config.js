cef35e1b9c846b9a9317930e83990785054451b0
jmau949 <44676775+jmau949@users.noreply.github.com>
2025-06-06 19:15:40 -0500

    web

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,25 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        '@tamagui/babel-plugin',
+        "module-resolver",
         {
-          components: ['@tamagui/core', '@tamagui/config', '@tamagui/button', '@tamagui/input', '@tamagui/text', '@tamagui/stacks', '@tamagui/card'],
-          config: './src/tamagui.config.ts',
-          logTimings: true,
-          disableExtraction: process.env.NODE_ENV === 'development',
-        },
-      ],
-      [
-        'module-resolver',
-        {
-          root: ['.'],
+          root: ["."],
           alias: {
-            '@': './src',
-            '@/components': './src/components',
-            '@/screens': './src/screens',
-            '@/hooks': './src/hooks',
-            '@/utils': './src/utils',
-            '@/types': './src/types',
-            '@/constants': './src/constants',
-            '@/services': './src/services',
-            '@/navigation': './src/navigation',
+            "@": "./src",
+            "@/components": "./src/components",
+            "@/screens": "./src/screens",
+            "@/hooks": "./src/hooks",
+            "@/utils": "./src/utils",
+            "@/types": "./src/types",
+            "@/constants": "./src/constants",
+            "@/services": "./src/services",
+            "@/navigation": "./src/navigation",
           },
         },
       ],
     ],
   };
-}; 
+};
